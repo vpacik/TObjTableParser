99a54a169864cb3f8207da24c2f2d309515151bd
Vojtech Pacik <vojta.pacik@gmail.com>
2018-11-22 13:13:21 +0100

    Plotting multiple parsed files impl'd

--- plot_parser.py
+++ plot_parser.py
@@ -14,32 +14,46 @@ print("###### Parser ######")
 # filepath = "test/out_perEvent_10"
 # filepath = "test/out_perEvent_100"
 # filepath = "test/out_hook_all_20"
-filepath = "/Users/vpacik/Codes/ALICE/Flow/uniFlow/class/misc/testMem/out_gridtest_wCor_all_full"
+# filepath = "/Users/vpacik/Codes/ALICE/Flow/uniFlow/class/misc/testMem/out_gridtest_wCor_all_full"
 # filepath = "/Users/vpacik/Codes/ALICE/Flow/uniFlow/class/misc/testMem/gridTest/stdout"
 
-# parsing raw input
-# instances = parse_file(filepath,stats=False)
-instances = parse_file(filepath,stats=False,hook="TObjectTable::")
-list_instances_total = [ i.total for i in instances ]
+filepaths = [
+    "/Users/vpacik/Codes/ALICE/Flow/uniFlow/class/misc/testMem/out_gridtest_wCor_all_0",
+    "/Users/vpacik/Codes/ALICE/Flow/uniFlow/class/misc/testMem/out_gridtest_wCor_all_100",
+    "/Users/vpacik/Codes/ALICE/Flow/uniFlow/class/misc/testMem/out_gridtest_wCor_all_full"
+]
 
+frames = []
 
-# getting list of total 'sizeHeap' for plotting
-list_sizeHeap = [ i.sizeHeap for i in list_instances_total]
-if gDebug : print(list_sizeHeap)
+for path in filepaths :
+    # parsing raw input
+    # instances = parse_file(filepath,stats=False)
+    instances = parse_file(path,stats=False,hook="TObjectTable::")
 
-list_notes = [ i.note for i in instances]
+    x = [ i for i in range(0,len(instances)) ]
+    list_instances_total = [ i.total for i in instances ]
+    list_notes = [ i.note for i in instances]
 
-# translating list_sizeHeap into [x,y]
-x = [ i for i in range(0,len(instances)) ]
+    # getting individual variables for plotting
+    list_sizeHeap = [ i.sizeHeap for i in list_instances_total]
+    if gDebug : print(list_sizeHeap)
 
-# converting data to DataFrame
-d = { 'index' : x, 'sizeHeap' : list_sizeHeap, 'notes' : list_notes }
-if gDebug : print(d)
+    list_countHeap = [ i.countHeap for i in list_instances_total]
+    if gDebug : print(list_countHeap)
 
-df = pd.DataFrame(d)
-print(df.head())
+    # converting data to DataFrame
+    d = { 'index' : x, 'sizeHeap' : list_sizeHeap, 'countHeap' : list_countHeap, 'notes' : list_notes }
+    if gDebug : print(d)
 
-sns.scatterplot(x='index', y='sizeHeap',data=df,hue='notes')
+    df = pd.DataFrame(d)
+    print(df.head())
+
+    frames.append(df)
+
+f, axes = plt.subplots(1, 3, sharey=True, figsize=(6, 4))
+sns.scatterplot(x='index', y='sizeHeap',data=frames[0],hue='notes',ax=axes[0])
+sns.scatterplot(x='index', y='sizeHeap',data=frames[1],hue='notes',ax=axes[1])
+sns.scatterplot(x='index', y='sizeHeap',data=frames[2],hue='notes',ax=axes[2])
 
 # # making diffs
 # list_diffs = []
